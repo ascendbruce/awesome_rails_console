d89313cc6d0f039183cca3dd079df8f2b55f0d8a
Bruce Li <ascendbruce@gmail.com>
2015-05-02 11:08:38 +0800

    Set up rspec

--- spec/awesome_pry_spec.rb
+++ spec/awesome_pry_spec.rb
@@ -5,7 +5,5 @@ describe AwesomePry do
     expect(AwesomePry::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  # TODO: Add more test or remove rspec entirely
 end
