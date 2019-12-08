b2c16dc807bc5e253a4b992e276afee0ac25c29d
Bruce Li <ascendbruce@gmail.com>
2019-12-08 22:59:09 +0800

    Fix deprecation warning in Rails 6.x
    fixes #10

--- lib/awesome_rails_console/railtie.rb
+++ lib/awesome_rails_console/railtie.rb
@@ -21,7 +21,13 @@ module AwesomeRailsConsole
     end
 
     def set_prompt_name_to_project_name
-      Pry.config.prompt_name = Rails.application.class.parent_name.underscore.dasherize
+      # ref from https://github.com/rweng/pry-rails/blob/035d5c8203521f5eaac0001300152281c765df88/lib/pry-rails/prompt.rb
+      Pry.config.prompt_name =
+        if Rails::VERSION::MAJOR >= 6
+          Rails.application.class.module_parent_name.underscore
+        else
+          Rails.application.class.parent_name.underscore
+        end
     end
 
     def show_rails_env_name_before_prompt
