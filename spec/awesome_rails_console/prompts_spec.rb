8727d28ef001b68ae38f30120199df05b19e4a1c
Matthew Nielsen <xunker@pyxidis.org>
2020-05-13 14:58:19 -0600

    test to ensure that correct pry prompt syntax is used depending on the current pry version

---
+++ spec/awesome_rails_console/prompts_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+require 'awesome_rails_console/prompts'
+
+describe AwesomeRailsConsole::Prompts do
+  let(:old_prompt) { double('Pry.config.prompt') }
+
+  before do
+    expect(Pry.config).to receive(:prompt).and_return(old_prompt)
+  end
+
+  context 'pry version < 0.13.0' do
+    before do
+      stub_const("Pry::VERSION", '0.12.2')
+    end
+
+    it 'uses pre-0.13.0 prompt' do
+      expect(described_class).to receive(:pre_pry_13_prompt).with(old_prompt)
+      described_class.choose_prompt_for_pry_version
+    end
+  end
+
+  context 'pry version >= 0.13.0' do
+    before do
+      stub_const("Pry::VERSION", '0.13.1')
+    end
+
+    it 'uses post-0.13.0 prompt' do
+      expect(described_class).to receive(:post_pry_13_prompt).with(old_prompt)
+      described_class.choose_prompt_for_pry_version
+    end
+  end
+end
